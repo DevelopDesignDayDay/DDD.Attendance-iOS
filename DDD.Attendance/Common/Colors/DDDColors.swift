b367f4bf598bf443ad6fcdf84fdf7c6e43de953f
Jaewook <hjw0911@gmail.com>
2021-07-25 20:30:03 +0900

    feat: DDDColor 추가

---
+++ DDD.Attendance/Common/Colors/DDDColors.swift
@@ -0,0 +1,14 @@
+//
+//  DDDColors.swift
+//  DDD.Attendance
+//
+//  Created by 황재욱 on 2021/07/25.
+//  Copyright © 2021 DDD. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  static let lightGray: UIColor = UIColor(red: 158/255, green: 159/255, blue: 163/255, alpha: 1)
+}
