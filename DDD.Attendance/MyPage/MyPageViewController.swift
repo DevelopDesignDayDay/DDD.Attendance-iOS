ef06d49f21e1df433e3c0d9e9a14b132ea9646ef
Jaewook <hjw0911@gmail.com>
2021-07-25 20:47:55 +0900

    feat: MyPageViewController 추가

---
+++ DDD.Attendance/MyPage/MyPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageViewController.swift
+//  DDD.Attendance
+//
+//  Created by 황재욱 on 2021/07/13.
+//  Copyright © 2021 DDD. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
