ea4ebe593f978b622a39aa338b5a61d4a23a0808
Jaewook <hjw0911@gmail.com>
2021-07-25 20:50:27 +0900

    chore: 필요한 color 추가

--- DDD.Attendance/Common/Colors/DDDColors.swift
+++ DDD.Attendance/Common/Colors/DDDColors.swift
@@ -11,4 +11,8 @@ import UIKit
 extension UIColor {
   
   static let lightGray: UIColor = UIColor(red: 158/255, green: 159/255, blue: 163/255, alpha: 1)
+  
+  static let darkBlack: UIColor = UIColor(red: 21/255, green: 21/255, blue: 21/255, alpha: 1)
+  
+  static let softGray: UIColor = UIColor(red: 145/255, green: 146/255, blue: 150/255, alpha: 1)
 }
