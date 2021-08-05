cd6801c9fa7eb1efcdc3bc5f150772ea092241f3
Jaewook <hjw0911@gmail.com>
2021-08-06 01:48:48 +0900

    feat: SignUpInfo model 추가

---
+++ DDD.Attendance/Domain/Entities/SignUpInfo.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpInfo.swift
+//  DDD.Attendance
+//
+//  Created by 황재욱 on 2021/08/06.
+//  Copyright © 2021 DDD. All rights reserved.
+//
+
+import Foundation
+
+struct SignUpInfo {
+  let token: String
+  let type: SocialAuthType
+  let email: String
+  let lastName: String
+  let firstName: String
+  let position: String
+}
